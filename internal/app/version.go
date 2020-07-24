0d12c058e01aca35fb3ee9cb5d0e816d4816fcc3
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-07-25 08:37:21 +0900

    Add version command

---
+++ internal/app/version.go
@@ -0,0 +1,23 @@
+package app
+
+import (
+	"github.com/moutend/go-radiko/pkg/radiko"
+	"github.com/spf13/cobra"
+)
+
+var versionCommand = &cobra.Command{
+	Use:     "version",
+	Aliases: []string{"v"},
+	Short:   "print version",
+	RunE:    recCommandRunE,
+}
+
+func versionCommandRunE(cmd *cobra.Command, args []string) error {
+	cmd.Printf("%s-%s\n", radiko.Version, radiko.Commit)
+
+	return nil
+}
+
+func init() {
+	RootCommand.AddCommand(versionCommand)
+}
---
+++ pkg/radiko/version.go
@@ -0,0 +1,9 @@
+package radiko
+
+const (
+	// Version is a version of this package.
+	Version = "v0.3.0"
+
+	// Commit is a commit hash of this package.
+	Commit = "latest"
+)
